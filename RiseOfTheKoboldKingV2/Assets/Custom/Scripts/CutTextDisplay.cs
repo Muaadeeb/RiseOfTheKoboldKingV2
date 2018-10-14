7facec50ace80325047701734e4810593278cae6
Hermanoid <lucasniewohner@gmail.com>
2018-10-14 14:10:52 -0500

    Create Cutscene logic

---
+++ RiseOfTheKoboldKingV2/Assets/Custom/Scripts/CutTextDisplay.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Image))]
+public class CutTextDisplay : MonoBehaviour
+{
+    Image Image { get; set; }
+    Text TextDisplay { get; set; }
+    string TextToDisplay { get; set; }
+    public float Delay = 0.05f;
+    public Action onComplete;
+    void Start()
+    {
+        Image = GetComponent<Image>();
+        Image.canvasRenderer.SetAlpha(0.0f);
+        TextDisplay = transform.GetChild(0).GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    public void ShowText(string TextToDisplay)
+    {
+        TextDisplay.text = "";
+        StartCoroutine(TypeWriterEffect(TextToDisplay));
+    }
+    public void HideText()
+    {
+        StartCoroutine(FadeOut());
+    }
+    IEnumerator TypeWriterEffect(string TextToDisplay)
+    {
+        float fadeInTime = 1.5f;
+        Image.CrossFadeAlpha(1.0f, fadeInTime, false);
+        yield return new WaitForSeconds(fadeInTime);
+        for (int i = 0; i < TextToDisplay.Length; i++)
+        {
+            TextDisplay.text = TextToDisplay.Substring(0, i);
+            yield return new WaitForSeconds(Delay);
+        }
+        onComplete();
+    }
+    IEnumerator FadeOut()
+    {
+        float fadeOutTime = 2.5f;
+        Image.CrossFadeAlpha(0.0f, fadeOutTime, false);
+        yield return new WaitForSeconds(fadeOutTime);
+    }
+}
---
+++ RiseOfTheKoboldKingV2/Assets/Custom/Scripts/SplashFade.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Collections;
+using System.Text;
+
+public class SplashFade : MonoBehaviour
+{
+    public CutTextDisplay[] cutTextDisplays;
+    public string loadLevel;
+    public string CutSceneName;
+    public int displayAtOnce = 2;
+    private int currentIndex = -1;
+    CutSceneScript script;
+
+
+    void Start()
+    {
+        foreach (var cutText in cutTextDisplays) cutText.onComplete += NextCut;
+        var file = Resources.Load<TextAsset>("JSON\\" + CutSceneName);
+        script = Newtonsoft.Json.JsonConvert.DeserializeObject<CutSceneScript>(Encoding.UTF8.GetString(file.bytes));
+        NextCut();
+    }
+
+    void NextCut()
+    {
+        currentIndex++;
+        if (currentIndex >= script.Cuts.Length)
+        {
+            SceneManager.LoadScene(loadLevel);
+        }
+        else
+        {
+            int toShow = currentIndex % cutTextDisplays.Length;
+            cutTextDisplays[toShow].ShowText(script.Cuts[currentIndex]);
+            if (displayAtOnce > 0)
+            {
+                int toHide = toShow - displayAtOnce;
+                toHide = toHide < 0 ? toHide + cutTextDisplays.Length : toHide;
+                cutTextDisplays[toHide].HideText();
+            }
+        }
+    }
+ 
+}
